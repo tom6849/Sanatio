89353c1e6ec5ea40c5710c0060c0409c0211bea0
E222897E <leo.chaumont@etu.univ-nantes.fr>
2024-10-16 10:21:57 +0200

    chore: change config and update dependancies

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,11 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
+  tabWidth: 4,
+  semi: true,
+  useTabs: false,
+  endOfLine: "lf"
 };
