7aaeb7f906612c25258f91155e2e699eaa51ce09
E226411Z <tom.berthemet@etu.univ-nantes.fr>
2025-01-22 20:58:13 +0100

    Correctif page Home

--- src/screenHome/components/MedicationRecap.tsx
+++ src/screenHome/components/MedicationRecap.tsx
@@ -1,43 +1,49 @@
 import React from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import Medicaments from './Medicaments';
+import LinearGradient from 'react-native-linear-gradient';
 
 const MedicationRecap = () => {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Aujourd'hui</Text>
-            <ScrollView style={styles.info} horizontal  showsHorizontalScrollIndicator={false}>
+            <ScrollView style={styles.info} horizontal  showsHorizontalScrollIndicator={false} contentContainerStyle={styles.scrollContent}>
                 <View style={styles.medicament}>
                     <Medicaments />
                 </View>
                 <View style={styles.medicament}>
                     <Medicaments />
                 </View>
-                <View style={styles.medicament2}>
+                <View style={styles.medicament}>
                     <Medicaments />
                 </View>
             </ScrollView>
+            
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 0.7,
+        height : '40%',
     },
     title: {
         color : "#0073C5",
         fontSize : 18,
-        marginBottom: 8,
     },
     info: {
         flexDirection: 'row',
+        flex : 1, 
+        paddingLeft : 10,
+    },
+    scrollContent: {
+        justifyContent: 'center', 
+        alignItems: 'center',    
     },
     medicament: {
         marginRight: 16,
     },
-    medicament2: {
-    }
+    
 });
 
 export default MedicationRecap;
--- src/screenHome/components/QuickAction.tsx
+++ src/screenHome/components/QuickAction.tsx
@@ -5,8 +5,6 @@ import Action from './Action';
 const QuickAction = () => {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Actions rapides</Text>
-            <View style={styles.info}>
                 <View style={styles.row}>
                     <View style={styles.box}>
                         <Text style={styles.boxTitle}>Ajouter une ordonnance</Text>
@@ -21,24 +19,21 @@ const QuickAction = () => {
                 <Text style={styles.boxTitle}>Signaler un effet secondaire </Text>
                      <Action logo="effet" />
                 </View>
-            </View>
+               
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        marginTop: 32,
+        height : '70%',
+        paddingTop : 32,
     },
-    info: {
-        flex: 1,
-        borderRadius: 10,
-    },
+    
     row: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        marginBottom: 12,
+        paddingBottom: 12,
     },
     box: {
         width: '48%',
@@ -59,11 +54,6 @@ const styles = StyleSheet.create({
         padding: 10, 
         elevation: 5,
     },
-    title: {
-        color: '#0073C5',
-        fontSize: 20,
-        marginBottom: 16,
-    },
     boxTitle: {
         color: 'white',
         fontSize: 16, 
@@ -74,6 +64,7 @@ const styles = StyleSheet.create({
         textShadowOffset: { width: 1, height: 2 },
         textShadowRadius: 6,
     },
+    
 });
 
 
