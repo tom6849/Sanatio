fe2c8b6de475587182388874fa11a7794c49ee73
E226411Z <tom.berthemet@etu.univ-nantes.fr>
2025-02-05 11:52:38 +0100

    Correction Page Pharmacy

--- src/screenPharmarcie/PharmacieScreen.tsx
+++ src/screenPharmarcie/PharmacieScreen.tsx
@@ -1,84 +1,122 @@
-import React from 'react';
-import { View, Text, StyleSheet, Pressable } from 'react-native';
-import Choice from './components/Choice';
-import { createStackNavigator } from '@react-navigation/stack';
-import LinearGradient from 'react-native-linear-gradient';
-import SearchMed from './components/SearchMed';
+import React, { useState, useRef } from 'react';
+import { StyleSheet, Text, View, ScrollView, Dimensions, SafeAreaView, TouchableOpacity } from 'react-native';
 import OcrScanner from './components/ScanOrd';
+import SearchMed from './components/SearchMed';
+import ImgScan from '../img/ImgScan';
+import ImgStock from '../img/ImgStock';
+import ImgSearchMed from '../img/ImgSearchMed';
 
-const Stack = createStackNavigator();
+const PharmacyMain = ({ navigation }: { navigation: any }) => {
+  const [pagePosition, setPagePosition] = useState(1);
+  const scrollViewRef = useRef<ScrollView>(null);
+  const pageWidth = Dimensions.get('window').width;
 
-const PharmacyMain = ({ navigation }: { navigation: any }) => (
-    <LinearGradient 
-            colors={['#4D82F3', '#C9E0F8']}
-            style={styles.container}
-            start={{ x: 0.9, y: 0 }}
-        >
-        <Pressable
-            style={styles.choiceButton}
-            onPress={() => navigation.navigate('ScanScreen')}
-        >
-            <Choice value={1} />
-        </Pressable>
-        <Pressable
-            style={styles.choiceButton}
-            onPress={() => navigation.navigate('SearchMedScreen')}
-        >
-            <Choice value={2} />
-        </Pressable>
-    </LinearGradient>
-);
+  const onScrollHandler = (event: any) => {
+    const scrollViewAbscissa = event.nativeEvent.contentOffset.x;
+    const newPagePosition = Math.round(scrollViewAbscissa / pageWidth) + 1;
+    if (newPagePosition !== pagePosition) {
+      setPagePosition(newPagePosition);  
+    }
+  };
 
-const Pharmacy = () => {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen
-                name="PharmacyMain"
-                component={PharmacyMain}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="ScanScreen"
-                component={OcrScanner}
-                options={{
-                    headerTitle: '',
-                    headerBackTitleVisible: false,
-                    headerStyle: { backgroundColor: '#F3F6FB' },
-                }}
-            />
-            <Stack.Screen
-                name="SearchMedScreen"
-                component={SearchMed} 
-                options={{
-                    headerTitle: '',
-                    headerBackTitleVisible: false,
-                    headerStyle: { backgroundColor: '#F3F6FB' },
-                }}
-            />
-        </Stack.Navigator>
-    );
+  const onIconPress = (index: number) => {
+    if (scrollViewRef.current) {
+        scrollViewRef.current.scrollTo({ x: index * pageWidth, animated: true });
+      }
+  };
+
+  const circleBackgroundColor = ['transparent', 'transparent', 'transparent'];
+  circleBackgroundColor[pagePosition - 1] = '#FFFFFF';
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.positionIndicatorsRow}>
+        <View style={styles.contours}>
+          {circleBackgroundColor.map((color, index) => (
+            <TouchableOpacity key={index} onPress={() => onIconPress(index)} style={[styles.positionIndicatorCircle, { backgroundColor: color }]}>
+              {index === 0 && <ImgSearchMed size={40} color={color === '#FFFFFF' ? '#4D82F3' : '#FFFFFF'} />}
+              {index === 1 && <ImgScan size={40} color={color === '#FFFFFF' ? '#4D82F3' : '#FFFFFF'} />}
+              {index === 2 && <ImgStock size={40} color={color === '#FFFFFF' ? '#4D82F3' : '#FFFFFF'} />} 
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
+        
+      <View style={styles.container1}>
+        <ScrollView
+          ref={scrollViewRef}
+          style={styles.onboardingScrollView}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          onScroll={onScrollHandler}
+          scrollEventThrottle={16}
+        >
+          <View style={styles.onboardingView}>
+            <SearchMed />
+          </View>
+            
+          <View style={styles.onboardingView}>
+            <OcrScanner />
+          </View>
+            
+          <View style={styles.onboardingView}>
+            <Text style={styles.pageText}>Page 3</Text>
+          </View>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
+  );
 };
 
-// Styles
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: '7%',
-        backgroundColor: '#F3F6FB',
-        alignItems:"center",
-        gap:"100%"
-    },
-    title: {
-        fontSize: 28,
-        fontWeight: 'bold',
-        color: '#002467',
-        textAlign: 'center',
-        marginBottom: 20,
-    },
-    choiceButton: {
-        flex: 1,
-        width:"80%",
-    },
+  container: {
+    flex: 1,
+  },
+  container1: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  onboardingScrollView: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+  },
+  onboardingView: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  pageText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  positionIndicatorsRow: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: '#F3F6FB',
+  },
+  contours: {
+    flex: 1, 
+    padding: 5, 
+    flexDirection: 'row',
+    justifyContent: 'center',
+    backgroundColor: '#7DA4F6',
+    borderRadius: 20,
+  },
+  positionIndicatorCircle: {
+    flex: 1,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+  },
 });
 
-export default Pharmacy;
+export default PharmacyMain;
--- src/screenPharmarcie/components/SearchMed.tsx
+++ src/screenPharmarcie/components/SearchMed.tsx
@@ -113,6 +113,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     borderColor: '#CCC',
     borderWidth: 1,
+    width : '100%'
   },
   searchText: {
     fontSize: 16,
